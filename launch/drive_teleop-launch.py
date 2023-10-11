ef637a402798544c34fa6202b70cf4182ff5779f
eduardohufg <eduardochavezmartin10@gmail.com>
2023-10-11 00:54:04 -0600

    joy implementation

--- launch/drive_teleop-launch.py
+++ launch/drive_teleop-launch.py
@@ -1,9 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 import launch_ros.actions
+import launch
+
 
 def generate_launch_description():
+    joy_dev = launch.substitutions.LaunchConfiguration('joy_dev')
+
     return LaunchDescription([
+        launch.actions.DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
         Node(
             package='simple_drive2',
             executable='simple_drive_teleop',
@@ -15,6 +20,7 @@ def generate_launch_description():
             executable='joy_node',
             name='joy_node',
             parameters=[{
+                'dev': joy_dev,
                 'deadzone': 0.3,
                 'autorepeat_rate': 20.0,
             }])
